16afd13d3c57e5fc77ec5a882891c6b2b2f222c9
Anitasilvi <anitaferdina06@gmail.com>
2023-12-25 05:04:28 +0700

    functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import Navigation from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
+import { useState } from "react";
+import products from './db/data'
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [query, setQuery] = useState("")
+  const handleInputChange = event => {
+    setQuery(event.target.value)
+  }
   return (
     <>
       <Sidebar/>
