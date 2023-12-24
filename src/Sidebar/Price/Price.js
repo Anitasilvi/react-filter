301122d29ee94a7e3da5a10443d531b2be5f86c6
Anitasilvi <anitaferdina06@gmail.com>
2023-12-24 23:13:54 +0700

    price section

--- src/Sidebar/Price/Price.js
+++ src/Sidebar/Price/Price.js
@@ -2,7 +2,29 @@ import React from 'react'
 import "./Price.css"
 function Price() {
   return (
-    <div>Price</div>
+    <div className='ml'>
+      <h2 className='sidebar-title price-title'>Price</h2>
+      <label className='sidebar-label-container'>
+          <input type="radio" name="test2" />
+          <span className='checkmark'></span> $0 - $50
+        </label>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test2" />
+          <span className='checkmark'></span> $50 - $100
+        </label>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test2" />
+          <span className='checkmark'></span> $100 - $150
+        </label>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test2" />
+          <span className='checkmark'></span>Over $50
+        </label>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test2" />
+          <span className='checkmark'></span> $0 - $50
+        </label>
+2    </div>
   )
 }
 
