b64bf95bfc787bfd844a99b7af8d783b2ddb84d6
Anitasilvi <anitaferdina06@gmail.com>
2023-12-24 23:04:08 +0700

    category section

--- src/Sidebar/Category/Category.js
+++ src/Sidebar/Category/Category.js
@@ -2,7 +2,31 @@ import React from 'react'
 import "./Category.css"
 function Category() {
   return (
-    <div>Category</div>
+    <div>
+      <h2 className='sidebar-title'>Category</h2>
+      <div>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test" />
+          <span className='checkmark'></span> All
+        </label>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test" />
+          <span className='checkmark'></span> Sneakers
+        </label>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test" />
+          <span className='checkmark'></span> Sandals
+        </label>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test" />
+          <span className='checkmark'></span> Heels
+        </label>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test" />
+          <span className='checkmark'></span> All
+        </label>
+      </div>
+    </div>
   )
 }
 
