cf4610184429aa0446191ad0b211c0cf08dcaade
Anitasilvi <anitaferdina06@gmail.com>
2023-12-25 19:33:27 +0700

    input & category component refactor

--- src/Sidebar/Price/Price.js
+++ src/Sidebar/Price/Price.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Price.css"
 import Input from '../../components/Input'
-function Price(handleChange) {
+function Price() {
   return (
     <div className='ml'>
       <h2 className='sidebar-title price-title'>Price</h2>
